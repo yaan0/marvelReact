28aee6476c5085c236083bc0c18053d0fa49f6e2
Yasha <yasha8600@bk.ru>
2022-06-27 17:47:28 +0500

    first commit

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,20 @@
+import { Helmet } from "react-helmet";
+
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+
+const ComicsPage = () => {
+  return (
+    <>
+      <Helmet>
+        <meta name="description" content="page with list of our comics" />
+        <title>Comics page</title>
+      </Helmet>
+
+      <AppBanner />
+      <ComicsList />
+    </>
+  );
+};
+
+export default ComicsPage;
